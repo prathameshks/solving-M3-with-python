d08c56798dcf99f4dea3bab7553f7844e634278f
Prathamesh <prathameshks2003@gmial.com>
2023-05-13 17:17:51 +0530

    gauss seidal done

--- 2 gauss seidal iterative method.py
+++ 2 gauss seidal iterative method.py
@@ -12,33 +12,46 @@ c3 = float(input("Enter c3: "))
 d3 = float(input("Enter d3: "))
 acu = int(input("Enter the accuracy: "))
 
-def jacobis(x0, y0, z0, itr):
-    x1 = (d1 - (b1 * y0) - (c1 * z0)) / a1
-    y1 = (d2 - (a2 * x0) - (c2 * z0)) / b2
-    z1 = (d3 - (a3 * x0) - (b3 * y0)) / c3
-    print(f"x{itr} = ", round(x1, acu))
-    print(f"y{itr} = ", round(y1, acu))
-    print(f"z{itr} = ", round(z1, acu))
-    return (x1, y1, z1)
+def gauss_seidal(x, y, z, itr):
+    x = (d1 - (b1 * y) - (c1 * z)) / a1
+    y = (d2 - (a2 * x) - (c2 * z)) / b2
+    z = (d3 - (a3 * x) - (b3 * y)) / c3
+    print(f"x{itr} = ", round(x, acu))
+    print(f"y{itr} = ", round(y, acu))
+    print(f"z{itr} = ", round(z, acu))
+    return (x, y, z)
 
-x1 = y1 = z1 = 0
+x = y = z = 0
 i = 1
 while (input("Do you want to continue? (y/n) ") != "n"):
     print("Iteration ", i)
-    x1, y1, z1 = jacobis(x1, y1, z1, i)
+    x, y, z = gauss_seidal(x, y, z, i)
     i += 1
 
 """
-10
+20
 1
+-2
+17
+3
+20
 -1
-11.19
-1
-10
-1
-28.08
+-18
+2
+-3
+20
+25
+
+27
+6
 -1
+85
 1
-10
-35.61
+1
+54
+110
+6
+15
+2
+72
 """
